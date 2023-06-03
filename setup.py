bd9a9cccc795b2727bfd27ac96c4abec4180e576
Dmitry Kalinin <kalinin.mitko@gmail.com>
2023-06-03 19:46:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="redfish_hp_ilo",
-    version="0.0.1",
+    version="0.0.2",
     description="Redfish API implementation on HPE servers with iLO RESTful API,",
     keywords="python, hp ilo, rest api, hp, ipmi",
     author="Kalinin Dmitry <kalinin.mitko@gmail.com>",
