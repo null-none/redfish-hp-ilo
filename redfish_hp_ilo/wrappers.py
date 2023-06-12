f0b1e519fe70fe3685d54a4f137709c10171f86a
Dmitry Kalinin <kalinin.mitko@gmail.com>
2023-06-12 22:39:45 +0300

    Update wrappers.py

--- redfish_hp_ilo/wrappers.py
+++ redfish_hp_ilo/wrappers.py
@@ -2,12 +2,12 @@ class ResponseWrapper:
     def __init__(self):
         pass
 
-    def render(self, response, data):
-        if response.status >= 200 and response.status < 300:
+    def result(self, response, data):
+        if response.status <= 200 and response.status < 300:
             return {"status": "success", "data": data}
         else:
             return {
                 "status": "error",
                 "message": data,
-                "status_code": response.status_code,
+                "status_code": response.status,
             }
